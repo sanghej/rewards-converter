9bcc523927c77a19998edec17374d425341f4d93
Jashandeep Sanghera <sanghej@uw.edu>
2023-05-20 00:03:03 -0700

    Added RewardValue class. Program now completed.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cash;
+    private int miles;
+
+    RewardValue(double x) {
+        this.cash = x;
+        this.miles = (int)(x / 0.0035);
+    }
+
+    RewardValue(int y) {
+        this.miles = y;
+        this.cash = y * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return miles;
+    }
 }
